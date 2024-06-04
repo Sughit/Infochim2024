73a797fa1e887d4acc5942ecce488090f336dbba
emanuel5867 <130173546+emanuel5867@users.noreply.github.com>
2024-06-04 23:29:46 +0300

    a

--- word-list.js
+++ word-list.js
@@ -23,5 +23,33 @@ const wordList = [
         word: "vapori",
         hint: "Moleculele de apă sunt mobile și au existență independentă atunci când apa e în stare de..."
     },
+    {
+        word: "djibouti",
+        hint: "Care este țara cu cele mai puține resurse interne de apă?"
+    },
+    {
+        word: "solubilitatea",
+        hint: "Cum se numește cantitatea maximă a unei substanțe ce poate fi dizolvată la o anumită temperatură în 100g solvent?"
+    },
+    {
+        word: "urina",
+        hint: "Prin ce se elimină cea mai multă apă din corp?"
+    },
+    {
+        word: "fizica",
+        hint: "Deversarea unor materiale insolubile este un tip de poluare..."
+    },
+    {
+        word: "zero",
+        hint: "Temperatura de îngheț a apei pure este de ...°C"
+    },
+    {
+        word: "insipid",
+        hint: "Apa pură este un lichid incolor, inodor și..."
+    },
+    {
+        word: "covalente",
+        hint: "Molecula de apă este formată din doi atomi de hidrogen și unul de oxigen, uniți prin două legături ... simple."
+    },
     
 ];
